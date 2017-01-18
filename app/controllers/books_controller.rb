72ec12b182303825e2d025c9681ae0397bc3ad3c
davygora <davygora.e@gmail.com>
2017-01-18 16:24:36 +0200

    add cover for book, create actions&views index, create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,30 @@
 class BooksController < ApplicationController
 
+  before_filter :authorize, only: [:new ]
+
   def index
-    Book.all
+    @books = Book.all
+  end
+
+  def new
+    @book = current_user.books.new
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to root_url, notice: 'Book create!'
+    else
+      render 'new'
+    end
+  end
+
+  # def destroy
+    
+  # end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :author, :summary, :cover)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117231357) do
+ActiveRecord::Schema.define(version: 20170117235803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,13 @@ ActiveRecord::Schema.define(version: 20170117231357) do
     t.string   "title"
     t.string   "author"
     t.string   "summary"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
