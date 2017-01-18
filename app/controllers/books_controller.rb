3cb7ffd1d1fe728899e7c57a472aa6d4f3fbf444
davygora <davygora.e@gmail.com>
2017-01-19 00:35:14 +0200

    add categories for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,6 @@ class BooksController < ApplicationController
 
 private
   def book_params
-    params.require(:book).permit(:title, :author, :summary, :cover)
+    params.require(:book).permit(:title, :author, :summary, :cover, { category_ids: [] })
   end
 end
