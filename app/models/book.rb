adea062db8ce1438d0e374cb197b380b842c9267
davygora <davygora.e@gmail.com>
2017-01-20 03:04:21 +0200

    add validations & update view

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,12 +2,18 @@ class Book < ActiveRecord::Base
 
   belongs_to :user
   has_and_belongs_to_many :categories
+
   has_attached_file :cover, styles: { small: '150x150'}
-  validates_attachment_content_type :cover, content_type: ["image/jpg", "image/jpeg", "image/png"]
 
   scope :last_week, -> { where("created_at >= ?", 7.days.ago.beginning_of_day) }
   scope :drafts, -> { where(draft: true) }
   scope :not_drafts, -> { where(draft: :false) }
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :cover, presence: true
+  validates :summary, presence: true, length: { in: 10..255 }
+  validates_attachment_content_type :cover, content_type: ["image/jpg", "image/jpeg", "image/png"]
 
   def owner?(other_user)
     user == other_user
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
 
   has_and_belongs_to_many :books
+  validates :title, presence: true
 end
